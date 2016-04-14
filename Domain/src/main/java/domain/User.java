b3af2041596d8d91fbaec6677c9e1940438bb6a0
Axel Olivecrona <axel.olivecrona1995@gmail.com>
2016-04-14 11:57:13 +0200

    Chaged interface to machted what was discussed at thursday metting 2

--- Domain/src/main/java/domain/User.java
+++ Domain/src/main/java/domain/User.java
@@ -3,35 +3,11 @@ package domain;
 /**
  * Created by robertkrook on 4/13/16.
  */
-public class User {
+public class User extends Person{
 
-    private String username;
-    private String name;
-    private String email;
     private boolean privileged_user;
 
-    public User(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Either gives or removes privileges.
-     * @return true if the user is given privileges, or false if privileges are removed.
-     */
-    public boolean privileges() {
-        privileged_user = true;
-        return privileged_user;
-    }
-
-    /**
-     * See if this user is an admin
-     * @return true if user is admin, else false
-     */
-    public boolean admin() {
-        return privileged_user;
+    public User(String email,String name,String password) {
+        super(password,name,email);
     }
 }
