5ee98e2db9a07b59323afb95d83803f8d1b20d16
Robert Krook <oamannen@yahoo.com>
2016-04-26 10:53:19 +0200

    initial implementation

--- Domain/src/main/java/domain/Repos/LocalCourseRepo.java
+++ Domain/src/main/java/domain/Repos/LocalCourseRepo.java
@@ -1,39 +1,91 @@
 package domain.Repos;
 
 import domain.Admin;
+import domain.Course;
 import domain.Matched;
 import domain.User;
 import domain.interfaces.ICourseRepo;
 import domain.util.Gcode;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by oliv on 4/23/16.
  */
 public class LocalCourseRepo implements ICourseRepo{
 
-    @Override
-    public Admin getCourseAdmin(Gcode gcode) {
-        return null;
+    private final Map<Gcode, Course> courses;
+    private static LocalCourseRepo repo;
+
+    /**
+     * private constructor
+     */
+    private LocalCourseRepo() {
+        courses = new HashMap<>();
+    }
+
+    /**
+     * Singleton constructor for LocalCourseRepo
+     * @return the single repository object
+     */
+    public static synchronized LocalCourseRepo getInstance() {
+        if(repo == null) {
+            repo = new LocalCourseRepo();
+            return repo;
+        } else {
+            return repo;
+        }
     }
 
     @Override
-    public List<User> getAllEnrolled(Gcode gcode) {
-        return null;
+    public String getCourseAdmin(Gcode gcode) {
+        return courses.get(gcode).getAdmin();
     }
 
     @Override
-    public void createCourse(Admin admin, String name) {
+    public List<String> getAllEnrolled(Gcode gcode) {
+        return courses.get(gcode).getRegisteredEmails();
+    }
 
+    @Override
+    public void createCourse(String admin, String name) {
+        Gcode code = new Gcode();
+        Course course = new Course(admin, name, code);
+        courses.put(code, course);
     }
 
     @Override
     public List<Matched> getAllMatches(Gcode gcode) {
-        return null;
+        return courses.get(gcode).returnMatched();
     }
 
     @Override
-    public void matchWith(List<String> toMatch) {
+    public void matchWith(List<String> toMatch, Gcode course) {
+        courses.get(course).putMatchRequest(toMatch.get(0), toMatch.get(1));
+    }
+
+    @Override
+    public List<Gcode> getEnrolledIn(String email) {
+        List<Gcode> enrolled = new ArrayList<>();
+        for(Course c : courses.values()) {
+            for(String member : c.getRegisteredEmails()) {
+                if(member.equals(email)) {
+                    enrolled.add(c.getCode());
+                    break;
+                }
+            }
+        }
+        return enrolled;
+    }
+
+    @Override
+    public List<Gcode> getAllAdministrating(String email) {
+        List<Gcode> administrating = new ArrayList<>();
+        for(Course c : courses.values()) {
+            if(c.getAdmin().equals(email)) {
+                administrating.add(c.getCode());
+            }
+        }
+        return administrating;
     }
 }
