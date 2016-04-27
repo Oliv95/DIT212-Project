e1a4c426eb548d96bb8073cff4467498e0e48fb9
Simon Boij <simon@boij.net>
2016-04-27 14:20:43 +0200

    implemented the create course server request

--- Client/app/src/main/java/se/ice/client/utility/ServerRequestService.java
+++ Client/app/src/main/java/se/ice/client/utility/ServerRequestService.java
@@ -4,6 +4,17 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -40,15 +51,25 @@ public class ServerRequestService extends Service implements Domain {
 
     @Override
     public Gcode createCourse(String courseName, String adminEmail) {
-        try {
-            String query = String.format("name=%s&email=%s",
-                    URLEncoder.encode(courseName, charset),
-                    URLEncoder.encode(adminEmail, charset));
-        } catch (Exception e) {
 
+        URL url = null;
+        try {
+            url = new URL(String.format(server + courses + "name=%s&email=%s",
+                    URLEncoder.encode(courseName, charset),
+                    URLEncoder.encode(adminEmail, charset)));
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Gcode courseCode = null;
+            courseCode = mapper.readValue(url, Gcode.class);
+            return courseCode;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return null;
+
+
     }
 
     @Override
