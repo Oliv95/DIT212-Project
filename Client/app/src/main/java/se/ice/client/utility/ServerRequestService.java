de9417a59d4588f53f367ece02f5418486573ca6
oboij <simon@boij.net>
2016-05-03 11:30:46 +0200

    added REST calls to server, but can't test. No response from Jonathan

--- Client/app/src/main/java/se/ice/client/utility/ServerRequestService.java
+++ Client/app/src/main/java/se/ice/client/utility/ServerRequestService.java
@@ -3,20 +3,11 @@ package se.ice.client.utility;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import se.ice.client.models.Admin;
@@ -28,6 +19,7 @@ public class ServerRequestService extends Service implements Domain {
     private final String server = "localhost:8080";
     private final String users = "/users";
     private final String courses = "/courses";
+    private final String admins = "/admins";
     private final String charset = "UTF-8";
 
     public ServerRequestService() {
@@ -41,28 +33,62 @@ public class ServerRequestService extends Service implements Domain {
 
     @Override
     public String createUser(String email, String name, String password) {
+        try {
+            URL url = new URL(String.format(server + users + "name=%s&email=%s&password=%s",
+                    URLEncoder.encode(name, charset),
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(password, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            JsonNode node = mapper.readValue(url, JsonNode.class);
+
+            return node.path("email").asText();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     @Override
     public String createAdmin(String email, String name, String password) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + admins + "name=%s&email=%s&password=%s",
+                    URLEncoder.encode(name, charset),
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(password, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            JsonNode node = mapper.readValue(url, JsonNode.class);
+
+            return node.path("email").asText();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
     }
 
     @Override
     public Gcode createCourse(String courseName, String adminEmail) {
 
-        URL url = null;
         try {
-            url = new URL(String.format(server + courses + "name=%s&email=%s",
+           URL url = new URL(String.format(server + courses + "name=%s&email=%s",
                     URLEncoder.encode(courseName, charset),
                     URLEncoder.encode(adminEmail, charset)));
+
+
 
         ObjectMapper mapper = new ObjectMapper();
 
         Gcode courseCode = mapper.readValue(url, Gcode.class);
             return courseCode;
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -73,42 +99,158 @@ public class ServerRequestService extends Service implements Domain {
 
     @Override
     public boolean joinCourse(String generatedCourseCode, String user) {
-        return false;
+        try {
+            URL url = new URL(String.format(server + users + "name=%s&gcode=%s",
+                    URLEncoder.encode(user, charset),
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+
+            JsonNode node = mapper.readValue(url, JsonNode.class);
+
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
     }
 
     @Override
     public boolean sendMatchRequest(String senderEmail, String receiverEmail, String generatedCourseCode) {
-        return false;
+        try {
+            URL url = new URL(String.format(server + users + "sender=%s&receiver=%s&gcode=%s",
+                    URLEncoder.encode(senderEmail, charset),
+                    URLEncoder.encode(receiverEmail, charset),
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+
+            JsonNode node = mapper.readValue(url, JsonNode.class);
+
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public List<User> getMatchedWith(String email, String generatedCourseCode) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + courses + "email=%sgcode=%s",
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            // This should convert to a list with User
+            List<User> user = mapper.readValue(url, List.class);
+
+            return user;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public User getUser(String email) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + users + "name=%s",
+                    URLEncoder.encode(email, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+
+            User user = mapper.readValue(url, User.class);
+
+            return user;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public List<User> getAllUsers(String generatedCourseCode) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + courses + "gcode=%s",
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            // This should convert to a list with User
+            List<User> user = mapper.readValue(url, List.class);
+
+            return user;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public List<User> getMatchedWithMe(String email, String generatedCourseCode) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + users + "email=%s&gcode=%s",
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            // This should convert to a list with User
+            List<User> user = mapper.readValue(url, List.class);
+
+            return user;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public Course getCourse(String generatedCourseCode) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + courses + "gcode=%s",
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            Course course = mapper.readValue(url, Course.class);
+
+            return course;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public Admin getAdmin(String email) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + admins + "email=%s",
+                    URLEncoder.encode(email, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            Admin admin = mapper.readValue(url, Admin.class);
+
+            return admin;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
@@ -118,26 +260,104 @@ public class ServerRequestService extends Service implements Domain {
 
     @Override
     public boolean login(String email, String password) {
-        return false;
+        try {
+            URL url = new URL(String.format(server + users + "email=%s&password=%s",
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(password, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+
+            JsonNode node = mapper.readValue(url, JsonNode.class);
+
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean sendPartnerRequest(String generatedCourseCode, String fromEmail, String toEmail) {
-        return false;
+        try {
+            URL url = new URL(String.format(server + users + "from=%s&to=%s&gcode=%s",
+                    URLEncoder.encode(fromEmail, charset),
+                    URLEncoder.encode(toEmail, charset),
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+
+            JsonNode node = mapper.readValue(url, JsonNode.class);
+
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public List<User> getPartnerRequest(String email, String generatedCourseCode) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + users + "email=%s&gcode=%s",
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            // This should convert to a list with User
+            List<User> user = mapper.readValue(url, List.class);
+
+            return user;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public boolean respondToPartner(String fromEmail, String toEmail, String generatedCourseCode, boolean response) {
-        return false;
+        try {
+            URL url = new URL(String.format(server + users + "from=%s&to=%s&gcode=%s&response=%s",
+                    URLEncoder.encode(fromEmail, charset),
+                    URLEncoder.encode(toEmail, charset),
+                    URLEncoder.encode(generatedCourseCode, charset),
+                    URLEncoder.encode(Boolean.toString(response), charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+
+            JsonNode node = mapper.readValue(url, JsonNode.class);
+
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public User getPartner(String email, String generatedCourseCode) {
-        return null;
+        try {
+            URL url = new URL(String.format(server + users + "email=%s&gcode=%s",
+                    URLEncoder.encode(email, charset),
+                    URLEncoder.encode(generatedCourseCode, charset)));
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            // This should convert to a list with User
+            User user = mapper.readValue(url, User.class);
+
+            return user;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
